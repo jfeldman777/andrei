f1ecb3375265b49e1823bab07e58d9535a4cc6fc
jfeldman777 <jfeldman777@gmail.com>
2023-04-21 10:15:24 +0300

    role

--- andrei/jacob/models.py
+++ andrei/jacob/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Role(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
