d3aaf5f53bb092c60ec0254a46fdf1adfdecdc7f
jfeldman777 <jfeldman777@gmail.com>
2023-04-21 08:48:21 +0300

    key

--- andrei/andrei/settings.py
+++ andrei/andrei/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -19,9 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)*&f7cabzg-vhe@nloz09m=su4v5pj3z6w32md8#l+nt7a@*ip'
-
+# SECURITY WARNING: keep the secret key used in production secret!SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
