91310a81a8e1c8972dff905ec8c36f7d078957cb
jfeldman777 <jfeldman777@gmail.com>
2023-05-07 16:58:36 +0300

    yes3

--- andrei/jacob/models.py
+++ andrei/jacob/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 class Role(models.Model):
     title = models.CharField(max_length=30)
-
+    general = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name='he',default=1)
     def __str__(self):
         return self.title
 
