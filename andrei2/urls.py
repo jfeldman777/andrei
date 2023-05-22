615f0769f98b696f9faa2b8f49fc133fc8c6fbd0
jfeldman777 <jfeldman777@gmail.com>
2023-05-22 18:27:30 +0300

    fire!

--- andrei2/urls.py
+++ andrei2/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jacob.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
