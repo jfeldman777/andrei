ead0770ec4c210c0a4f591c50e60eaf28eb99282
jfeldman777 <jfeldman777@gmail.com>
2023-05-22 12:51:44 +0300

    fire!

--- andrei2/settings.py
+++ andrei2/settings.py
@@ -95,15 +95,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-
+    #os.path.join(BASE_DIR, 'staticfiles'),
+    BASE_DIR /'staticfiles'
  ]
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-print(STATIC_ROOT)
-print(os.path.join(BASE_DIR, 'staticfiles'),)
+
 
 if IS_HEROKU:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
