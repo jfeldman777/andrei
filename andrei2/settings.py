b435197839c64c1d65e70d5feefd7b4b7d275888
jfeldman777 <jfeldman777@gmail.com>
2023-05-22 19:12:54 +0300

    fire!

--- andrei2/settings.py
+++ andrei2/settings.py
@@ -96,7 +96,7 @@ AUTH_PASSWORD_VALIDATORS = [
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [    
                             os.path.join(BASE_DIR, 'static'),
-                            os.path.join(BASE_DIR, 'jacob/static'),
+                            os.path.join(BASE_DIR, 'jacob.static'),
     
 
  ]
