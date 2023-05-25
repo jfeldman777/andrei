de469fcacef28406413f9c0b5a372c41e3441372
jfeldman777 <jfeldman777@gmail.com>
2023-05-25 15:01:38 +0300

    minor

--- jacob/vvv.py
+++ jacob/vvv.py
@@ -1077,9 +1077,9 @@ def dj(request, p, r, j):  # Дельта, один проект все ресу
                 color = "white"
                 if mj_outside(d, project):
                     color = "lightgrey"
-                    if delta[i+1] < 0:
+                    if delta[i] < 0:
                         color = "#B266FF"
-                elif delta[i+1] < 0:
+                elif delta[i] < 0:
                     color = "pink"                        
                 elif a_w3[i]>0:
                     color = "lightblue"                   
