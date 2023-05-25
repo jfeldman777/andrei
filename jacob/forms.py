795dd66757975ec4ea4146920041142c1ebd120c
jfeldman777 <jfeldman777@gmail.com>
2023-05-25 21:12:47 +0300

    minor

--- jacob/forms.py
+++ jacob/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import formset_factory
 from .models import Role,Project
+from django.contrib.admin.widgets import AdminDateWidget
 
 class LoadForm(forms.Form):
    role = forms.IntegerField(widget=forms.HiddenInput())
@@ -26,7 +27,18 @@ from django import forms
 from .models import Project
 
 class ProjectForm(forms.ModelForm):
+    start_date = forms.DateField(label="начало",widget=forms.SelectDateWidget)
+    end_date = forms.DateField(label="окончание",widget=forms.SelectDateWidget)
+    title = forms.CharField(label="название",max_length=30, widget=forms.TextInput(attrs={'size': '30'}))
+
     class Meta:
         model = Project
-        fields = ['id','title', 'general','start_date','end_date']
+        fields = ['id', 'title', 'general', 'start_date', 'end_date']
+        labels = {
+
+            'general': 'руководитель',
+
+        }
+
         # Дополнительные поля формы
+        
