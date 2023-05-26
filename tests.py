7e81e7b580f64cf8a91a7d92ea52dccfda656f94
jfeldman777 <jfeldman777@gmail.com>
2023-05-26 20:15:45 +0300

    Include db.sqlite3 file

---
+++ tests.py
@@ -0,0 +1,13 @@
+# myapp/tests.py
+from django.test import TestCase, Client
+
+class PageStatusTestCase(TestCase):
+    def test_home_page_status(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+#    def test_redirect_status(self):
+#        client = Client()
+#        response = client.get('/redirect-url/')
+#        self.assertEqual(response.status_code, 301)
