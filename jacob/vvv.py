c0324f93dfb6fe17b2929c77aa5a5331b354c3e3
jfeldman777 <jfeldman777@gmail.com>
2023-06-09 06:08:21 +0300

    bug

--- jacob/vvv.py
+++ jacob/vvv.py
@@ -17,7 +17,7 @@ from django.urls import resolve
 '''
 def atest(request:object)->object:
 
-    return render(request, "navig.html")
+    return render(request, "a00.html")
 
 def a00(request:object)->object:
     return render(request, "a00.html")
