3e4ef4ca81261316744c8bfd8fa4c9221c961c8a
jfeldman777 <jfeldman777@gmail.com>
2023-07-04 18:02:20 +0300

    login1

--- andrei2/settings.py
+++ andrei2/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["andrei2.herokuapp.com", "andrei.herokuapp.com",
-                 "pmlogix-847d27334089.herokuapp.com",
-                 "andrei3-3336cc517a01.herokuapp.com",
+                 "pmlogicx.pro",
                  "127.0.0.1", "testserver", "localhost"]
 #
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
