09a932f98ba4c81de3eeb74be0db38e940bda10e
jfeldman777 <jfeldman777@gmail.com>
2023-07-04 16:23:37 +0300

    login1

--- andrei2/settings.py
+++ andrei2/settings.py
@@ -24,7 +24,7 @@ if IS_HEROKU:
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
-ALLOWED_HOSTS = ["andrei2.herokuapp.com", "andrei.herokuapp.com",
+ALLOWED_HOSTS = ["andrei2.herokuapp.com", "andrei.herokuapp.com","andrei3-3336cc517a01.herokuapp.com",
                  "127.0.0.1", "testserver", "localhost"]
 #
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
